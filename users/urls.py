384477e17c936bc08d57751b39fb0a00e53c7f6a
alex1331 <danilin999@bk.ru>
2023-07-14 10:37:44 +0300

    Скорректирован функционал лайков и дизлайков,+ добавлена страница пользователя, где отображаются его посты и личная информация

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from users.views import Register
-from . import views
+from .views import profile
 
 urlpatterns = [
     path('home/', include('django.contrib.auth.urls')),
     path('register/', Register.as_view(), name='register'),
+    path('profile/', profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from news.models import NewsPost
 from users.forms import UserCreationForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
@@ -33,6 +36,25 @@ class Register(View):
         return render(request, self.template_name, context)
 
 
+
+@login_required
+def profile(request):
+    user = request.user
+    posts = NewsPost.objects.filter(user=user)
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserCreationForm(instance=user)
+
+    context = {
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'profile.html', context)
 
 
 
